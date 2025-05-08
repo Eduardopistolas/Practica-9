a9ba75ce34719f01894b1b3b8aad0a652b6f7543
Eduardopistolas <eduardogranados0@comunidad.unam.mx>
2025-05-08 14:24:26 -0600

    Create Ejercicio2.c

---
+++ Ejercicio2.c
@@ -0,0 +1,21 @@
+//Hacer el promedio de 5 calificaciones 
+
+    #include <stdio.h>
+
+    void main ()
+{
+    int list[5];
+    int i, calif= 0;
+    float prom;
+    for (i= 0; i < 5; i++)
+{
+    printf("Introducir la calificacion numero %d: ",i+1);
+    scanf("%d", &list[i]);
+}
+    for (i= 0; i < 5; i++)
+    {
+    calif= calif+list[i];
+}
+    prom= calif/5;
+    printf("EL promedio de las calificaciones es: %f\n", prom);
+}
